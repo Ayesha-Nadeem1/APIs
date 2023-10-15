00ab502766fa08cde27ecdf1ed809cc7a05c290d
Ayesha Nadeem <ashnadeem123@gmail.com>
2023-10-15 20:58:17 +0500

    add user controller

---
+++ userController.js
@@ -0,0 +1,56 @@
+const User = require('../models/user');
+
+async function createUser(req,res){
+    try{
+        //console.write(req.body);
+        const user = await User.create(req.body);
+        res.status(201).json(user);//for successful req: response code 200 ki line me
+    }
+    catch (err){
+        res.status(500).json({error: err.message});//for unsuccessful req: response code 500 ki line me
+    }
+}
+
+async function getAllUsers(req,res){
+    try{
+        const users = await User.find();
+        res.json(users);
+    }
+    catch (err){
+        res.status(500).json({error: err.message});
+    }
+}
+
+
+
+async function updateUser(req, res) {
+    try
+    {
+        const {id} = req.params;
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true }); 
+        res.json(updatedUser);
+    } 
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+async function deleteUser(req, res) {
+    try
+    {
+        const {id} = req.params;
+        await User.findByIdAndRemove(id); 
+        res.sendStatus(204);
+    } 
+    catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+
+module.exports = {
+    createUser,
+    getAllUsers,
+    updateUser,
+    deleteUser,
+};
